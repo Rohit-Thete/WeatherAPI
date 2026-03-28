6326c1c0f731161768ce7d24d5de976b6d3773fc
Rohit Thete <rohitthete.512@gmail.com>
2026-03-28 12:24:05 +0530

    added home page

---
+++ weatherAPI/views.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeView(APIView):
+    def get(self,request):
+        return Response({
+            "message":"Welcome to Weather API",
+            "endpoints":["/monthly","/seasonal","/annual"]
+        })
