9d1c7fd4e6309da8e2e7e26afb3ca67f3149c1be
Rohit Thete <rohitthete.512@gmail.com>
2026-04-08 18:01:10 +0530

    Fixed the parser

--- api/utils.py
+++ api/utils.py
@@ -33,7 +33,7 @@ def load_data(region_name, parameter_name):
             continue
 
         year_value = int(col[0])
-        year,_=Year.objects.get_or_create(year = year_value)
+        year,_ =Year.objects.get_or_create(year = year_value)
 
 
 
