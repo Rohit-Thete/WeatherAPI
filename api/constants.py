f4a30f51bda3be3e86e182d1119052f67f49c323
Rohit Thete <rohitthete.512@gmail.com>
2026-04-08 09:58:18 +0530

    Added Constants File

---
+++ api/constants.py
@@ -0,0 +1,27 @@
+MONTH_CHOICES =[
+    ("Jan","January"),("Feb","February"),("Mar","March"),
+    ("Apr","April"),("May","May"),("Jun","June"),
+    ("Jul","July"),("Aug","August"),("Sep","September"),
+    ("Oct","October"),("Nov","November"),("Dec","December")]
+
+
+
+SEASON_CHOICES=[("Win","Winter"),("Spr","Spring"),("Sum","Summer"),("Aut","Autumn")]
+
+
+
+PARAMETER_UNITS = {
+    "Tmax": "°C",
+    "Tmin": "°C",
+    "Tmean": "°C",
+    "Rainfall": "mm",
+    "Sunshine": "hours",
+    "Raindays1mm": "days",
+    "AirFrost": "days",
+}
+
+MONTHS = ["jan", "feb", "mar", "apr", "may", "jun",
+          "jul", "aug", "sep", "oct", "nov", "dec"]
+
+SEASONS = ["win", "spr", "sum", "aut"]
+   
